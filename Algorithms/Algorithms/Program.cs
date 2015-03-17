0f0c4c2253eceb07f2e65ced96cb731d9a50e932
HamritSidhu <hamritsidhu_94@hotmail.com>
2015-03-16 23:37:20 -0400

    integer to string conversion algorithm

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 
-
-
 namespace Algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
+            stringToIntConversion conversion = new stringToIntConversion();
+            Console.WriteLine("Enter a string to convert:");
+            string convertee = Console.ReadLine();
+            conversion.convert(convertee);
+            Console.ReadLine();
         }
     }
 }
--- Algorithms/Algorithms/stringToIntConversion.cs
+++ Algorithms/Algorithms/stringToIntConversion.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 /**
  * Problem Definition: Convert a string to integer 
@@ -8,5 +9,57 @@ namespace Algorithms
 {
     class stringToIntConversion
     {
+        public int getInt(string value) {
+             ASCIIEncoding encode = new ASCIIEncoding();
+             byte byteZero = encode.GetBytes("0")[0];
+             byte[] charAsciis = encode.GetBytes(value);
+             int result = 0;
+             int power = 0;
+             int add = 0;
+             for (int i = charAsciis.Length - 1; i >= 0; i--)
+             {
+                 add = (charAsciis[i] - byteZero) * (int)Math.Pow(10, power);
+                 result += add;
+                 power++;
+             }
+
+             return result;
+        }
+
+        public bool isInt(string value) {
+            ASCIIEncoding encode = new ASCIIEncoding();
+
+            byte lowestInt = encode.GetBytes("0")[0];
+            byte highestInt = encode.GetBytes("9")[0];
+
+            byte[] charAsciis = encode.GetBytes(value);
+            foreach (byte b in charAsciis)
+            {
+                if (b > highestInt || b < lowestInt)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public void convert(string value) {
+            if (value != null && value.Length > 0)
+            {
+                bool convertable = this.isInt(value);
+                Console.WriteLine("Input String: " + value);
+                if (convertable)
+                {
+                    int converted = this.getInt(value);
+                    Console.WriteLine("Output integer: " + converted);
+                }
+                else
+                    Console.WriteLine("The string cannot be converted to an integer");
+
+            }
+            else 
+                Console.WriteLine("Invalid string");
+            
+        }
+
     }
 }
